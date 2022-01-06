04fd21d95972ea64302b18ca31810fab3c9f893d
Iain <iainmilliken@gmail.com>
2022-01-05 20:04:39 -0700

    added new seed data and gitignore

--- client/server/config/seeds.js
+++ client/server/config/seeds.js
@@ -5,11 +5,9 @@ db.once('open', async () => {
   await Category.deleteMany();
 
   const categories = await Category.insertMany([
-    { name: 'Food' },
-    { name: 'Household Supplies' },
-    { name: 'Electronics' },
-    { name: 'Books' },
-    { name: 'Toys' }
+    { name: "TV's and Projectors" },
+    { name: "Laptops and PC's" },
+    { name: 'Cellphones and Tablets' },
   ]);
 
   console.log('categories seeded');
@@ -18,112 +16,86 @@ db.once('open', async () => {
 
   const products = await Product.insertMany([
     {
-      name: 'Tin of Cookies',
+      name: 'Amazon - 43" Class Omni Series 4K UHD Smart Fire TV hands-free with Alexa',
       description:
         'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
-      image: 'cookie-tin.jpg',
+      image: '6479709_sd.jpg',
       category: categories[0]._id,
-      price: 2.99,
-      quantity: 500
+      price: 409.99,
+      quantity: 32
     },
     {
-      name: 'Canned Coffee',
+      name: 'Epson - EpiqVision™ Mini EF12 Smart Streaming Laser Projector with HDR and Android TV - Black and Copper',
       description:
         'Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.',
-      image: 'canned-coffee.jpg',
+      image: '6428489_sd.jpg',
       category: categories[0]._id,
-      price: 1.99,
+      price: 999.99,
       quantity: 500
     },
     {
-      name: 'Toilet Paper',
+      name: 'Samsung - 75" Class 8000 Series LED 4K UHD Smart Tizen TV', 
+      description: "Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.",
+      image: '6453027_sd.jpg',
+      category: categories[0]._id,
+      price: 1199.99,
+      quantity: 8
+
+    },
+    {
+      name: 'Apple - MacBook Pro - 16" Display with Touch Bar - Intel Core i9 - 16GB Memory - AMD Radeon Pro 5500M - 1TB SSD - Silver',
       category: categories[1]._id,
       description:
         'Donec volutpat erat erat, sit amet gravida justo sodales in. Phasellus tempus euismod urna. Proin ultrices nisi ut ipsum congue, vitae porttitor libero suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam lacinia a nisi non congue.',
-      image: 'toilet-paper.jpg',
-      price: 7.99,
+      image: '6366581_sd.jpg',
+      price: 2799.99,
       quantity: 20
     },
     {
-      name: 'Handmade Soap',
+      name: 'Skytech Gaming - Shiva Gaming PC Desktop – AMD Ryzen 5 5600X – 16G Memory – NVIDIA GeForce RTX 3060 Ti – 1TB NVME – 120mm AIO - White',
       category: categories[1]._id,
       description:
         'Praesent placerat, odio vel euismod venenatis, lectus arcu laoreet felis, et fringilla sapien turpis vestibulum nisl.',
-      image: 'soap.jpg',
-      price: 3.99,
-      quantity: 50
+      image: '6482938_sd.jpg',
+      price: 1649.99,
+      quantity: 3
     },
     {
-      name: 'Set of Wooden Spoons',
+      name: 'ASUS - ROG Zephyrus M16 GU603 Gaming Laptop - Intel Core i9 - 16GB Memory - NVIDIA RTX3060 - 1TB SSD - Off Black',
       category: categories[1]._id,
       description:
         'Vivamus ut turpis in purus pretium mollis. Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
       image: 'wooden-spoons.jpg',
-      price: 14.99,
-      quantity: 100
+      price: 1849.99,
+      quantity: 10
     },
     {
-      name: 'Camera',
+      name: 'OtterBox - Commuter Series for Apple® iPhone® 12 mini - Black',
       category: categories[2]._id,
       description:
         'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
-      image: 'camera.jpg',
-      price: 399.99,
-      quantity: 30
+      image: '6422363_rd.jpg',
+      price: 11.99,
+      quantity: 80
     },
     {
-      name: 'Tablet',
+      name: 'Microsoft - Surface Go 2 - 10.5" Touch-Screen - Intel Pentium Gold - 8GB - 128GB SSD - Device Only - Platinum',
       category: categories[2]._id,
       description:
         'In sodales, ipsum quis ultricies porttitor, tellus urna aliquam arcu, eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris justo ante, aliquet non tempus in, tempus ac lorem. Aliquam lacinia dolor eu sem eleifend ultrices. Etiam mattis metus metus. Sed ligula dui, placerat non turpis vitae, suscipit volutpat elit. Phasellus sagittis, diam elementum suscipit fringilla, libero mauris scelerisque ex, ac interdum diam erat non sapien.',
-      image: 'tablet.jpg',
-      price: 199.99,
-      quantity: 30
+      image: '6408475_sd.jpg',
+      price: 579.99,
+      quantity: 20
     },
     {
-      name: 'Tales at Bedtime',
-      category: categories[3]._id,
+      name: 'Amazon - All-New Fire HD 10 – 10.1” – Tablet – 32 GB - Black',
+      category: categories[2]._id,
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.',
       image: 'bedtime-book.jpg',
-      price: 9.99,
-      quantity: 100
+      price: 149.99,
+      quantity: 70
     },
-    {
-      name: 'Spinning Top',
-      category: categories[4]._id,
-      description: 'Ut vulputate hendrerit nibh, a placerat elit cursus interdum.',
-      image: 'spinning-top.jpg',
-      price: 1.99,
-      quantity: 1000
-    },
-    {
-      name: 'Set of Plastic Horses',
-      category: categories[4]._id,
-      description:
-        'Sed a mauris condimentum, elementum enim in, rhoncus dui. Phasellus lobortis leo odio, sit amet pharetra turpis porta quis.',
-      image: 'plastic-horses.jpg',
-      price: 2.99,
-      quantity: 1000
-    },
-    {
-      name: 'Teddy Bear',
-      category: categories[4]._id,
-      description:
-        'Vestibulum et erat finibus erat suscipit vulputate sed vitae dui. Ut laoreet tellus sit amet justo bibendum ultrices. Donec vitae felis vestibulum, congue augue eu, finibus turpis.',
-      image: 'teddy-bear.jpg',
-      price: 7.99,
-      quantity: 100
-    },
-    {
-      name: 'Alphabet Blocks',
-      category: categories[4]._id,
-      description:
-        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
-      image: 'alphabet-blocks.jpg',
-      price: 9.99,
-      quantity: 600
-    }
   ]);
 
   console.log('products seeded');
